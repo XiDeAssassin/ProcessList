90929ded7edc78be066f8614492c73a108a43445
XinoAssassin Ni <phnjs211@outlook.com>
2017-06-11 17:07:19 +0800

    添加项目文件。

---
+++ ProcessList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ProcessList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processes = Process.GetProcesses();
+            string[] processNames = new string[processes.Length];
+            int i = 0;
+            foreach (Process p in processes)
+            {
+                if (i<=processes.Length)
+                {
+                    processNames[i] = p.ProcessName;
+                    i++;
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            foreach(string s in processNames)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
